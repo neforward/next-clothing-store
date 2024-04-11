dcb2fa28e2b18233571a79ced91e30f243d338ee
neforward <141821349+neforward@users.noreply.github.com>
2024-04-11 18:26:25 +0600

    mmm

--- src/app/collab/page.tsx
+++ src/app/collab/page.tsx
@@ -1,5 +1,26 @@
 export default function Collaboration() {
-  return <>
-  
-  </>;
+  return (
+    <>
+      <div className="collaboration">
+        <div className="container">
+          <div className="collab">
+            <h2>
+              We are happy to meet new people and always open to collaboration
+              or discussions.
+            </h2>
+            <p>
+              If you are a fashion brand, digital designer, influencer, or just
+              a good person who wants to reach us and make something cool
+              together do not heist to contact us!
+            </p>
+            <div className="collab-mail">
+              <span>OUR EMAIL</span>
+              <h5>info@verseser.com</h5>
+            </div>
+          </div>
+        </div>
+        <div className="gredient-line"></div>
+      </div>
+    </>
+  );
 }
