4e1942c8078d017d74424ae1c17056a9bc928ac5
neforward <141821349+neforward@users.noreply.github.com>
2024-04-11 19:24:25 +0600

    kkk

--- src/app/wear/page.tsx
+++ src/app/wear/page.tsx
@@ -1,5 +1,93 @@
 export default function Wear() {
-  return <>
-  
-  </>;
+  return (
+    <>
+      <div className="wear-page">
+        <div className="wear-top">
+          <div className="container">
+            <h2>CHOOSE THE RIGHT PICTURE</h2>
+            <p>RECOMMENDATIONS FOR RECEIVING THE BEST PHOTO LOOKS</p>
+          </div>
+          <div className="gredient-line"></div>
+        </div>
+        <div className="container">
+          <div className="wear-content">
+            <div className="wear-box">
+              <div className="wear-txt-sect">
+                <button>STEP 1</button>
+                <h3>NATURAL LIGHT</h3>
+                <ul>
+                  Make sure:
+                  <li>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      viewBox="0 0 16 16"
+                      fill="none"
+                    >
+                      <path
+                        d="M12.0229 3.77958C11.5056 3.2673 10.7924 3.16183 9.87333 3.16183H6.11161C5.20257 3.16183 4.4894 3.2673 3.9721 3.7846C3.4548 4.29688 3.35938 5.00503 3.35938 5.91407V9.67579C3.35938 10.5999 3.4548 11.308 3.9721 11.8203C4.4894 12.3376 5.20257 12.4381 6.12165 12.4381H9.87333C10.7924 12.4381 11.5056 12.3376 12.0229 11.8203C12.5402 11.303 12.6356 10.5999 12.6356 9.67579V5.92913C12.6356 5.00503 12.5402 4.29688 12.0229 3.77958Z"
+                        fill="#1C1C1E"
+                      />
+                    </svg>
+                    your picture is well-lit
+                  </li>
+                  <li>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      viewBox="0 0 16 16"
+                      fill="none"
+                    >
+                      <path
+                        d="M12.0229 3.77958C11.5056 3.2673 10.7924 3.16183 9.87333 3.16183H6.11161C5.20257 3.16183 4.4894 3.2673 3.9721 3.7846C3.4548 4.29688 3.35938 5.00503 3.35938 5.91407V9.67579C3.35938 10.5999 3.4548 11.308 3.9721 11.8203C4.4894 12.3376 5.20257 12.4381 6.12165 12.4381H9.87333C10.7924 12.4381 11.5056 12.3376 12.0229 11.8203C12.5402 11.303 12.6356 10.5999 12.6356 9.67579V5.92913C12.6356 5.00503 12.5402 4.29688 12.0229 3.77958Z"
+                        fill="#1C1C1E"
+                      />
+                    </svg>
+                    the light is even on a face and body
+                  </li>
+                </ul>
+                <ul>
+                  Avoid:
+                  <li>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      viewBox="0 0 16 16"
+                      fill="none"
+                    >
+                      <path
+                        d="M2.80213 8.96763L6.83003 13.0006C7.60347 13.769 8.397 13.774 9.16039 13.0106L13.2084 8.96261C13.9718 8.19922 13.9668 7.40569 13.1983 6.63225L9.17043 2.60435C8.397 1.83091 7.60347 1.82589 6.84008 2.5943L2.78706 6.6423C2.02367 7.40569 2.02869 8.19922 2.80213 8.96763Z"
+                        fill="#D65B54"
+                      />
+                    </svg>
+                    your picture is well-lit
+                  </li>
+                  <li>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      viewBox="0 0 16 16"
+                      fill="none"
+                    >
+                      <path
+                        d="M2.80213 8.96763L6.83003 13.0006C7.60347 13.769 8.397 13.774 9.16039 13.0106L13.2084 8.96261C13.9718 8.19922 13.9668 7.40569 13.1983 6.63225L9.17043 2.60435C8.397 1.83091 7.60347 1.82589 6.84008 2.5943L2.78706 6.6423C2.02367 7.40569 2.02869 8.19922 2.80213 8.96763Z"
+                        fill="#D65B54"
+                      />
+                    </svg>
+                    the light is even on a face and body
+                  </li>
+                </ul>
+              </div>
+              <div className="wear-pic-one"></div>
+              <div className="wear-pic-two"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
