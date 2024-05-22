fc0c1c40cc82a3e730441e7fe3d9f586ca600fe4
neforward <141821349+neforward@users.noreply.github.com>
2024-05-22 19:43:49 +0600

    kkk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
       <body className={`${inter.className}`}>
         <Header />
         <ReduxProvider>{children}</ReduxProvider>
